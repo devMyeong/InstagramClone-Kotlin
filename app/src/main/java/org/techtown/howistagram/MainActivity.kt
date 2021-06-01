9c84ea0d4d6ca17db781124ffc012ccb438d3c5c
DESKTOP-APA5QCM\imJ <imjb1641@gmail.com>
2021-06-01 17:46:49 +0900

    Photo upload function update
    - checkSelfPermission()
    - requestPermissions()
    - ACTION_PICK

--- app/src/main/java/org/techtown/howistagram/MainActivity.kt
+++ app/src/main/java/org/techtown/howistagram/MainActivity.kt
@@ -1,14 +1,16 @@
 package org.techtown.howistagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import org.techtown.howistagram.navigation.AlarmFragment
-import org.techtown.howistagram.navigation.DetailViewFragment
-import org.techtown.howistagram.navigation.GridFragment
-import org.techtown.howistagram.navigation.UserFragment
+import org.techtown.howistagram.navigation.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -25,6 +27,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
+
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm -> {
@@ -45,5 +51,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
 }
